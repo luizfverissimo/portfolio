c08060c5abea82ba0033bdd1b6ffd2f33a043387
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2020-12-19 17:02:12 -0300

    hero section DONE

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,75 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import Image from 'next/image';
+import Lottie from 'react-lottie';
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import styles from '../styles/home.module.scss';
+import animationData from '../public/lotties/lf30_editor_w6g77lum.json';
 
 export default function Home() {
+  const lottieOption = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData
+  };
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>LF Verissimo | FullStack Developer</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <main className={styles.content}>
+        <section className={styles.header}>
+          <header className={styles.navBar}>
+            <a>
+              <Image
+                src='/lf_verissimo_logo_light.svg'
+                width='173'
+                height='21'
+              />
+            </a>
+            <nav className={styles.navButtonsWrapper}>
+              <a>Home</a>
+              <a>Projects</a>
+              <a>About</a>
+              <a>Contact</a>
+            </nav>
+          </header>
+        </section>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href='https://github.com/vercel/next.js/tree/master/examples'
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <div className={styles.imgWrapper}>
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              width={150}
-              height={50}
-              layout='responsive'
-              className={styles.logo}
-            />
+        <section className={styles.hero}>
+          <div className={styles.heroContent}>
+            <div className={styles.heroText}>
+              <h1>
+                Hello!
+                <br /> I’m LF Verissimo,
+              </h1>
+              <p>Solving problems through code.</p>
+            </div>
+            <div className={styles.heroImg}>
+              <Lottie options={lottieOption} width={450} height={450} />
+              <span className={styles.bgCircle} />
+            </div>
           </div>
-        </a>
-      </footer>
-    </div>
+
+          <a className={styles.projectsBtn}>
+            <FontAwesomeIcon
+              icon={faAngleDoubleDown}
+              color='#fafafa'
+              width={24}
+              height={24}
+            />
+            <p>My Projects</p>
+          </a>
+        </section>
+        <section className={styles.projects}>
+          <div className={styles.projectRow}>
+            <h2>Offers Aggregator</h2>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
